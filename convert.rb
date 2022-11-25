f138a54811253437ba3788c21c66c7c0da8f96c3
Yohei Yasukawa <yohei@yasslab.jp>
2022-11-25 19:27:30 +0900

    Deploy proof of concept of Dojo Map

--- convert.rb
+++ convert.rb
@@ -1,13 +1,24 @@
+#!/usr/bin/env ruby
 require 'json'
 
-dojos = []
+dojos_earth = []
+dojos_japan = []
 
 File.open("dojos_earth.json") do |file|
-  dojos = JSON.load(file)
+  dojos_earth = JSON.load(file)
+end
+
+File.open("dojos_japan.json") do |file|
+  dojos_japan = JSON.load(file) #.map{|data| data.transform_keys!(&:to_sym) }
+end
+
+name2text = {}
+dojos_japan.each do |dojo|
+  name2text[dojo['name']] = "<a href='#{dojo['url']}' target='_blank' rel='noopener'>Webサイトを見る</a><br />"
 end
 
 features = []
-dojos.each do |dojo|
+dojos_earth.each do |dojo|
   # 活動していない道場は除外
   #
   # stage:
@@ -17,6 +28,10 @@ dojos.each do |dojo|
   # 3: 満員
   # 4: 活動していません
   if dojo["geoPoint"] && dojo["stage"] != 4
+    if dojo['name'] == 'Chofu'
+      dojo['name'].gsub!('Chofu', '調布')
+    end
+
     features << {
       "type" => "Feature",
       "geometry" => {
@@ -24,7 +39,7 @@ dojos.each do |dojo|
         "coordinates" => [dojo["geoPoint"]["lon"], dojo["geoPoint"]["lat"]]
       },
       "properties" => {
-        "description" => "#{dojo['name']}<br /><a target='_blank' href='http://zen.coderdojo.com/dojos/#{dojo['urlSlug']}'>zen</a>"
+        "description" => "#{dojo['name']}<br />#{name2text[dojo['name']]}<a target='_blank' href='http://zen.coderdojo.com/dojos/#{dojo['urlSlug']}'>zen</a>"
       }
     }
   end
