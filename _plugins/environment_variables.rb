48b348bcbbfdd21c11378c7c97f00e0f512979ba
Yohei Yasukawa <yohei@yasslab.jp>
2022-12-30 13:41:36 +0900

    Pass GEOLONIA_API_KEY via custom Jekyll plugin for easy local development

---
+++ _plugins/environment_variables.rb
@@ -0,0 +1,14 @@
+# GitHub Gist - Environment variables in Jekyll templates
+# https://gist.github.com/nicolashery/5756478
+
+# Plugin to add environment variables to the `site` object in Liquid templates
+
+module Jekyll
+  class EnvironmentVariables < Generator
+    def generate(site)
+      site.config['env'] = {}
+      site.config['env']['GEOLONIA_API_KEY'] = ENV['GEOLONIA_API_KEY'] || 'YOUR-API-KEY'
+      # Add other environment variables to `site.config` here...
+    end
+  end
+end
