a83ffc30de6e500bb1ff0708bb47787bee007012
Yohei Yasukawa <yohei@yasslab.jp>
2023-01-22 00:32:12 +0900

    Load Google Analytics only in production mode

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -8,6 +8,7 @@ module Jekyll
     def generate(site)
       site.config['env'] = {}
       site.config['env']['GEOLONIA_API_KEY'] = ENV['GEOLONIA_API_KEY'] || 'YOUR-API-KEY'
+      site.config['env']['JEKYLL_ENV']       = ENV['JEKYLL_ENV']       || 'development'
       # Add other environment variables to `site.config` here...
     end
   end
