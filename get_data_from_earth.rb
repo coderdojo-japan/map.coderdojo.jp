a928f0d94761eb4cb32e612253523bfd6142924a
Yohei Yasukawa <yohei@yasslab.jp>
2024-01-09 23:38:16 +0900

    Prototype: Fetch dojo data from GraphQL API (aka Clubs API)

--- get_data_from_earth.rb
+++ get_data_from_earth.rb
@@ -4,6 +4,93 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+## Example script for retrieving all CoderDojos in Japan
+
+API_URI = URI.parse('https://clubs-api.raspberrypi.org/graphql')
+
+HEADERS = {
+  'accept' => 'application/json',
+  'content-type' => 'application/json'
+}
+
+DOJOS_IN_COUNTRY_QUERY = <<~GRAPHQL
+  query (
+    $countryCode: String!,
+    $after: String,
+  ) {
+    clubs(
+      after: $after,
+      filterBy: {
+        countryCode: $countryCode,
+        brand: CODERDOJO,
+        verified: true
+      }
+    ) {
+      nodes {
+        name
+        latitude
+        longitude
+        countryCode
+        stage
+        urlSlug: url
+        startTime
+        endTime
+        openToPublic
+        frequency
+        day
+        id: uuid
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+GRAPHQL
+
+variables = {
+ countryCode: 'JP'
+}
+
+def request_data(variables:)
+  req_options = {
+    use_ssl: API_URI.scheme == 'https'
+  }
+  request      = Net::HTTP::Post.new(API_URI.request_uri, HEADERS)
+  request.body = { query: DOJOS_IN_COUNTRY_QUERY, variables: }.to_json
+
+  response = Net::HTTP.start(API_URI.hostname, API_URI.port, req_options) do |http|
+    http.request(request)
+  end
+
+  JSON.parse(response.body, symbolize_names: true)[:data][:clubs]
+end
+
+dojos = []
+
+loop do
+ fetched_data = request_data(variables:)
+
+ dojos    += fetched_data[:nodes]
+ page_info = fetched_data[:pageInfo]
+
+ break unless page_info[:hasNextPage]
+
+ variables[:after] = page_info[:endCursor]
+end
+
+File.open('dojos_earth.json', 'w') do |file|
+ file.puts(JSON.pretty_generate(dojos))
+end
+
+puts "\nNumber of dojos: #{dojos.length}"
+puts "\nCheck out JSON data you fetched by:"
+puts '$ cat dojos_data.json'
+
+exit
+
+################
+
 uri     = URI.parse("https://zen.coderdojo.com/api/2.0/dojos")
 request = Net::HTTP::Post.new(uri)
 request.content_type = "application/json"
